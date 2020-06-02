0f2fce25ae98bc1c7d6031a4a72a99ffff21d512
Kyungjin Josiah Park <KyungjinJPark@gmail.com>
2020-06-02 12:23:25 -0400

    placeholder topbar

---
+++ src/TopBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TopBar = () => {
+  return (
+    <div>
+      [RESHOES LOGO] ReShoes About FAQ
+      <button>Sign in</button>
+      <button>Sign out</button>
+    </div>
+  );
+}
+
+export default TopBar;
