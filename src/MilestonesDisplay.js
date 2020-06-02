bea6b0a5bc05bda20d213d55f43178624d12c847
Kyungjin Josiah Park <KyungjinJPark@gmail.com>
2020-06-02 12:39:53 -0400

    placeholder profile page

---
+++ src/MilestonesDisplay.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import MilestonesDisplay from "./MilestonesDisplay";
+
+const ProfilePage = () => {
+  return (
+    <ul>
+      <li>10pts </li>
+      <li>25pts </li>
+      <li>50pts </li>
+      <li>100pts </li>
+    </ul>
+  );
+}
+
+export default ProfilePage;
---
+++ src/ProfilePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import MilestonesDisplay from "./MilestonesDisplay";
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <p>Your Referral Code: !*(^!@(*(@#</p>
+      <MilestonesDisplay />
+      <p>Your Points: ##</p>
+      <p>Your Referrals: ##</p>
+    </div>
+  );
+}
+
+export default ProfilePage;
