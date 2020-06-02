501824d9e5cce61abe874feb35f8247f8d3384c4
Jack Chandler <jtc4hnu@virginia.edu>
2020-06-02 14:40:37 -0400

    React Router

--- src/App.js
+++ src/App.js
@@ -3,33 +3,57 @@ import './App.css';
 
 import AccountForm from "./accountForm.js";
 
+import firebase from "firebase";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   const [account, setAccount] = useState(false);
 
   return (
-    <div className="App">
-      <header>
-        <div className="BrandMarker">
-          <div>Company Name</div>
-          <img alt="Company Logo" />
-        </div>
+    <Router>
+      <div className="App">
+        <header>
+          <div className="BrandMarker">
+            <div>Company Name</div>
+            <img alt="Company Logo" />
+          </div>
+          <div className="SignInDisplay">
+            {
+              account ?
+                <div>
+                </div>
+                :
+                <div>
+                  <button onClick={() => setAccount({ appmode: "Account", create: false })}>Sign in </button>
+                  <button onClick={() => setAccount({ appmode: "Account", create: true })}>Create Account</button>
+                </div>
+            }
 
-        <button onClick={() => setAccount({ appmode: "Account", create: false })}>Sign in </button>
-        <button onClick={() => setAccount({ appmode: "Account", create: true })}>Create Account</button>
-      </header>
-      <div>CONTENT</div>
+          </div>
 
-      {
-        account.appmode &&
-        <AccountForm
-          Methods={{
-            Close: () => setAccount(false)
-          }}
-          Data={{
-            Create: account.create
-          }} />
-      }
-    </div>
+        </header>
+        <Switch>
+          <Route exact path="/signin"></Route>
+          <Route exact path="/about"></Route>
+          <Route exact path="/faq"></Route>
+          <Route exact path="/dashboard"></Route>
+          <Route exact path="/"></Route>
+        </Switch>
+        <div>CONTENT</div>
+
+
+        {
+          account.appmode &&
+          <AccountForm
+            Methods={{
+              Close: () => setAccount(false)
+            }}
+            Data={{
+              Create: account.create
+            }} />
+        }
+      </div>
+    </Router>
   );
 }
 
